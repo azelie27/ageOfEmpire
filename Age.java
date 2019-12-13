e23d2e9c7b1c087ea9358de5a8235c6bc9263204
fpeyg <frederiqueperrin@gmail.com>
2019-12-13 15:20:15 +0100

    v2

---
+++ Age.java
@@ -0,0 +1,18 @@
+class Age {
+
+    private String tool = "pierre";
+ 
+
+    public Age () {
+    }
+
+    public Age (String tool) {
+        this.tool = tool;
+    }
+
+    
+    public String toString() {
+        return "[outil]: " + this.tool;
+    }
+
+}
